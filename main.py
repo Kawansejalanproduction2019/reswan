2606b42d78dfb4249319cec8969a9bab445c1cee
Kawansejalanproduction2019 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2025-07-09 21:21:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -208,6 +208,7 @@ async def setup_hook():
     """Dipanggil sekali saat bot pertama kali startup."""
     print("ðŸ” Starting setup_hook and loading cogs...")
     await load_cogs()
+    await load_extension()
     print(f"✅ Finished setup_hook and all cogs attempted to load.")
     # Ini akan dicetak setelah semua cog dimuat, tetapi sebelum on_ready sepenuhnya selesai
     print(f"All commands registered: {[command.name for command in bot.commands]}")
