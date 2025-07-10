977c660cf7e6ac7d4c46cef7ab007c42c9419e5a
Kawansejalanproduction2019 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2025-07-10 20:40:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -355,7 +355,6 @@ async def load_cogs():
         "cogs.multigame",
         "cogs.dunia",
         "cogs.koruptor",
-        "cogs.psikotes",
         "cogs.tempvoice",
         "cogs.endgame",
         "cogs.psikotes"
@@ -372,8 +371,7 @@ async def load_cogs():
 async def setup_hook():
     """Called once when the bot first starts up."""
     log.info("🚀 Starting setup_hook and loading cogs...")
-    await load_cogs()
-    await load_extension() 
+    await load_cogs() 
     log.info(f"✅ Finished setup_hook and all cogs attempted to load.")
     # Log registered commands after cogs are loaded
     log.info(f"All commands registered: {[command.name for command in bot.commands]}")
